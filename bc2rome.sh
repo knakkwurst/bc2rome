bed1c285f7360bb934b71c1af2647b71bc3ee60d
Jan Hansen <janaha@gmail.com>
2024-04-05 15:58:19 +0000

    Update bc2rome.sh

--- bc2rome.sh
+++ bc2rome.sh
@@ -7,4 +7,4 @@ export DISPLAY=:1
 Xvfb :1 -screen 0 800x600x16 &
 x11vnc -bg -reopen -forever
 cd /project/R34_Full
-wine Frost.Game.Main_Win32_Final.exe -serverInstancePath "Instance/" -mapPack2Enabled 1 -port 19567 -timeStampLogNames -region OC -heartBeatInterval 20000
+wine Frost.Game.Main_Win32_Final.exe -serverInstancePath "Instance/" -mapPack2Enabled 1 -port 19567 -timeStampLogNames -region EU -heartBeatInterval 20000
